c441cc6044df221c439dd74b345846a82d26ab95
Philipp Millar <philipp@advancedtelematic.com>
2015-09-18 17:19:50 +0200

    Widen permissions of rvi certificates

--- rvi/client_cert.sh
+++ rvi/client_cert.sh
@@ -2,7 +2,7 @@
 python scripts/rvi_create_certificate.py \
   --id=f1630572-a977-4eff-907d-2a0a3c886d05 \
   --register="genivi.org/vin/+/sota/notify genivi.org/vin/+/sota/start genivi.org/vin/+/sota/chunk genivi.org/vin/+/sota/finish" \
-  --invoke="genivi.org/backend/sota/ack genivi.org/backend/sota/initiate_download" \
+  --invoke="genivi.org/backend/sota/+" \
   --root_key=sota_keys/root_key_priv.pem \
   --device_key=sota_keys/client_key_pub.pem \
   --jwt_out=sota_certs/client.certificate.jwt \
