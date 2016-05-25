9ccac3b95165dda6aa72f3fdf07c0b35d004f8cf
Jerry Trieu <jerry@advancedtelematic.com>
2016-05-25 14:42:21 +0200

    Update RVI to 0.5.0

--- rvi/certs/gen_keys.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-KEY_DIR=$PWD/sota_keys
-mkdir -p $KEY_DIR
-
-cd ${RVI_DIR:-rvi_core}
-scripts/rvi_create_root_key.sh -o $KEY_DIR/root -b 4096
-python scripts/rvi_create_device_key.py -p $KEY_DIR/root_priv.pem -b 4096 -o $KEY_DIR/server
-python scripts/rvi_create_device_key.py -p $KEY_DIR/root_priv.pem -b 4096 -o $KEY_DIR/client
