66d580a2a4d46504d767eb18a5510c563c231b6b
Robert Danitz <robert@advancedtelematic.com>
2018-02-07 11:48:46 +0100

    better debugging, readme, readability

--- bitbakery/run.sh
+++ bitbakery/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # create myself
-useradd --shell /bin/bash -u $USER_ID -o -c "" -m bitbake -d /home/bitbake
+useradd --shell /bin/bash -u $USER_ID -o -c "" -m -d /home/bitbake bitbake
 
 id
 exec sudo -u bitbake /bin/bash - << EOF
@@ -17,8 +17,12 @@ cp /opt/site.conf conf/
 # build image
 bitbake $BITBAKE_FLAGS $IMAGE
 result=$?
-cp $BUILD_DIR/tmp/deploy/images/$TARGET/$IMAGE-$TARGET.otaimg $OUT_DIR
-cp $BUILD_DIR/tmp/deploy/images/$TARGET/u-boot.* $OUT_DIR
-cp -r $BUILD_DIR/tmp/deploy/images/$TARGET/ostree_repo $OUT_DIR
+
+if [ $result -eq 0 ]; then
+  cp $BUILD_DIR/tmp/deploy/images/$TARGET/$IMAGE-$TARGET.otaimg $OUT_DIR
+  cp $BUILD_DIR/tmp/deploy/images/$TARGET/u-boot.* $OUT_DIR
+  cp -r $BUILD_DIR/tmp/deploy/images/$TARGET/ostree_repo $OUT_DIR
+fi
+
 exit $result
 EOF
