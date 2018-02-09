79eec26db869bb51c896121cc5936f5d4ae2a013
Robert Danitz <robert@advancedtelematic.com>
2018-02-09 10:15:40 +0100

    streamline config parameters
    - switch to debian stretch slim

--- bitbakery/bitbake.sh
+++ bitbakery/bitbake.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # create myself
 useradd --shell /bin/bash -u $USER_ID -o -c "" -m -d /home/bitbake bitbake
@@ -12,17 +12,12 @@ export HOME=/home/bitbake
 source $SRC_DIR/meta-updater/scripts/envsetup.sh $TARGET $BUILD_DIR
 
 # appending user-defined config
-cp /opt/site.conf conf/
+cp $CONF_DIR/site.conf conf/
 
 # build image
 bitbake $BITBAKE_FLAGS $IMAGE
-result=$?
 
-if [ $result -eq 0 ]; then
-  cp $BUILD_DIR/tmp/deploy/images/$TARGET/$IMAGE-$TARGET.otaimg $OUT_DIR
-  cp $BUILD_DIR/tmp/deploy/images/$TARGET/u-boot.* $OUT_DIR
-  cp -r $BUILD_DIR/tmp/deploy/images/$TARGET/ostree_repo $OUT_DIR
-fi
-
-exit $result
+cp $BUILD_DIR/tmp/deploy/images/$TARGET/$IMAGE-$TARGET.otaimg $OUT_DIR
+cp $BUILD_DIR/tmp/deploy/images/$TARGET/u-boot.* $OUT_DIR
+cp -r $BUILD_DIR/tmp/deploy/images/$TARGET/ostree_repo $OUT_DIR
 EOF
