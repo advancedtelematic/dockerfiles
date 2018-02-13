689be294a7b8eabf8972bbbe88ac6d8e11ab918d
Robert Danitz <robert@advancedtelematic.com>
2018-02-13 17:11:17 +0100

    not sourcing the bitbake script with a user
    - relies on running the docker container with the right user
    - remove bitbake flags for now

--- bitbakery/bitbake.sh
+++ bitbakery/bitbake.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 
-# create myself
-useradd --shell /bin/bash -u $USER_ID -o -c "" -m -d /home/bitbake bitbake
-
-id
-exec sudo -u bitbake /bin/bash - << EOF
-id
-
-export HOME=/home/bitbake
+set -x
 
 source $SRC_DIR/meta-updater/scripts/envsetup.sh $TARGET $BUILD_DIR
 
@@ -16,8 +9,15 @@ cp $CONF_DIR/site.conf conf/
 
 # build image
 bitbake $BITBAKE_FLAGS $IMAGE
+result=$?
 
-cp $BUILD_DIR/tmp/deploy/images/$TARGET/$IMAGE-$TARGET.otaimg $OUT_DIR
-cp $BUILD_DIR/tmp/deploy/images/$TARGET/u-boot.* $OUT_DIR
-cp -r $BUILD_DIR/tmp/deploy/images/$TARGET/ostree_repo $OUT_DIR
-EOF
+if [ $result -eq 0 ]; then
+  cp $BUILD_DIR/tmp/deploy/images/$TARGET/$IMAGE-$TARGET.otaimg \
+     $OUT_DIR
+  cp $BUILD_DIR/tmp/deploy/images/$TARGET/u-boot.* \
+     $OUT_DIR
+  cp -r $BUILD_DIR/tmp/deploy/images/$TARGET/ostree_repo \
+     $OUT_DIR
+fi
+
+exit $result
